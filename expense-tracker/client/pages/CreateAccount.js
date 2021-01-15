f1916f1d7f276cb4ed3fcc516d779af7494ec844
Tom Allain <tom.allain@protonmail.com>
2021-01-14 21:50:48 -0400

    auth form local states;

--- expense-tracker/client/pages/CreateAccount.js
+++ expense-tracker/client/pages/CreateAccount.js
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function CreateAccount() {
+    const [name, setName] = useState('');
+    const [password, setPassword] = useState('');
+
     return (
         <div>
             <h1 className='subtitle has-text-centered'>Create Account</h1>
@@ -11,7 +14,13 @@ function CreateAccount() {
                     <label className='label'>Name</label>
 
                     <div className='control'>
-                        <input className='input' type='text' />
+                        <input className='input'
+                            name='name'
+                            onChange={event => {
+                                event.preventDefault();
+                                setName(event.target.value);
+                            }} type='text'
+                            value={name} />
                     </div>
 
                     <p className='help is-danger'>Validation message goes here.</p>
@@ -21,14 +30,27 @@ function CreateAccount() {
                     <label className='label'>Password</label>
 
                     <div className='control'>
-                        <input className='input' type='text' />
+                        <input className='input'
+                            name='password'
+                            onChange={event => {
+                                event.preventDefault();
+                                setPassword(event.target.value);
+                            }} type='password'
+                            value={password} />
                     </div>
 
                     <p className='help is-danger'>Validation message goes here.</p>
                 </div>
 
                 <div className='control level has-text-centered py-5'>
-                    <button className='button is-success'>Create</button>
+                    <button className='button is-success'
+                        onClick={event => {
+                            event.preventDefault();
+                            console.log(name);
+                            console.log(password);
+                        }}>
+                        Create
+                    </button>
 
                     <div className='py-5'>
                         <a href='/'>Cancel</a>
--- expense-tracker/client/pages/Login.js
+++ expense-tracker/client/pages/Login.js
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Login() {
+    const [name, setName] = useState('');
+    const [password, setPassword] = useState('');
+
     return (
         <div>
             <h1 className='subtitle has-text-centered'>Login</h1>
@@ -11,7 +14,13 @@ function Login() {
                     <label className='label'>Name</label>
 
                     <div className='control'>
-                        <input className='input' type='text' />
+                        <input className='input'
+                            name='name'
+                            onChange={event => {
+                                event.preventDefault();
+                                setName(event.target.value);
+                            }} type='text'
+                            value={name} />
                     </div>
 
                     <p className='help is-danger'>Validation message goes here.</p>
@@ -21,14 +30,27 @@ function Login() {
                     <label className='label'>Password</label>
 
                     <div className='control'>
-                        <input className='input' type='text' />
+                        <input className='input'
+                            name='password'
+                            onChange={event => {
+                                event.preventDefault();
+                                setPassword(event.target.value);
+                            }} type='password'
+                            value={password} />
                     </div>
 
                     <p className='help is-danger'>Validation message goes here.</p>
                 </div>
 
                 <div className='control level has-text-centered py-5'>
-                    <button className='button is-link'>Login</button>
+                    <button className='button is-link'
+                        onClick={event => {
+                            event.preventDefault();
+                            console.log(name);
+                            console.log(password);
+                        }}>
+                        Login
+                    </button>
 
                     <div className='py-5'>
                         <a href='/create'>No account? Create one!</a>
